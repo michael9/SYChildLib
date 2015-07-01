fcccc3613f6afb5d11d5ddbff82de9c385fb83a7
michael carlos <michael82carlos@gmail.com>
2015-07-01 17:03:45 +0800

    002

--- src/com/cqvip/moblib/sychildlib/GuideActivity.java
+++ src/com/cqvip/moblib/sychildlib/GuideActivity.java
@@ -5,26 +5,73 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 
 public class GuideActivity extends Activity {
 
-	Button guide_btn01, guide_btn02, guide_btn03, guide_btn04;
+	View guide_btn01, guide_btn02, guide_btn03, guide_btn04;
+
+	AnimationSet am01,am02,am03,am04;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_guide);
-		guide_btn01 = (Button) findViewById(R.id.guide_btn01);
-		guide_btn02 = (Button) findViewById(R.id.guide_btn02);
-		guide_btn03 = (Button) findViewById(R.id.guide_btn03);
-		guide_btn04 = (Button) findViewById(R.id.guide_btn04);
+		guide_btn01 = (View) findViewById(R.id.guide_btn01);
+		guide_btn02 = (View) findViewById(R.id.guide_btn02);
+		guide_btn03 = (View) findViewById(R.id.guide_btn03);
+		guide_btn04 = (View) findViewById(R.id.guide_btn04);
+
+		am01= new AnimationSet(true);
+//		AlphaAnimation a=new AlphaAnimation(0,1);
+		ScaleAnimation sa1=new ScaleAnimation(0f,1f,0f,1f,Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF,0.5f);
+		sa1.setInterpolator(new DecelerateInterpolator());
+//		am01.addAnimation(a);
+		am01.addAnimation(sa1);
+		am01.setDuration(500);
+		am01.setStartOffset(100);
+
+		am02=new AnimationSet(true);
+		ScaleAnimation sa2=new ScaleAnimation(0f,1f,0f,1f,Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF,0.5f);
+		sa2.setInterpolator(new DecelerateInterpolator());
+		am02.addAnimation(sa2);
+		am02.setDuration(500);
+		am02.setStartOffset(1800);
+
+		am03=new AnimationSet(true);
+		ScaleAnimation sa3=new ScaleAnimation(0f,1f,0f,1f,Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF,0.5f);
+		sa3.setInterpolator(new DecelerateInterpolator());
+		am03.addAnimation(sa3);
+		am03.setDuration(500);
+		am03.setStartOffset(1300);
+
+		am04=new AnimationSet(true);
+		ScaleAnimation sa4=new ScaleAnimation(0f,1f,0f,1f,Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF,0.5f);
+		sa4.setInterpolator(new DecelerateInterpolator());
+		am04.addAnimation(sa4);
+		am04.setDuration(500);
+		am04.setStartOffset(800);
 
 		guide_btn01.setOnClickListener(btnclick);
 		guide_btn02.setOnClickListener(btnclick);
 		guide_btn03.setOnClickListener(btnclick);
 		guide_btn04.setOnClickListener(btnclick);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		guide_btn01.startAnimation(am01);
+		guide_btn02.startAnimation(am02);
+		guide_btn03.startAnimation(am03);
+		guide_btn04.startAnimation(am04);
+
 	}
 
 	OnClickListener btnclick = new OnClickListener() {
